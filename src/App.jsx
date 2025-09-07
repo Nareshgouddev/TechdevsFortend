8c937368de3e20a7946de295a5b75ad237e15748
Naresh goud <ediganareshgoud406@gmail.com>
2025-09-07 19:32:32 +0530

    reduxStrore

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Body";
+import Login from "./Login";
+import appStore from "./utils/appStrore";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/login" element={<div>LoginPage</div>} />
-        <Route path="/test" element={<div>TestPage</div>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={appStore}>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/test" element={<div>TestPage</div>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
