16a838466e5852042f55edeb3ce1c93a573b8113
Naresh goud <ediganareshgoud406@gmail.com>
2025-09-13 22:05:24 +0530

    profile

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { BASE_URL } from "../utils/constants";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>feed</div>;
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(`${BASE_URL}/feed`, {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  if (!feed || !Array.isArray(feed.users)) {
+    return <div>Loading...</div>;
+  }
+
+  if (feed.users.length === 0) {
+    return <h1 className="flex justify-center my-10">No new users found!</h1>;
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center my-10 gap-4">
+      {feed.users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
 };
 
 export default Feed;
